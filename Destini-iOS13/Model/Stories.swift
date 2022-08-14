c70a8c8f5568a5654857e9451c9a7faddb5b076a
Sidney So <buffalo704@hotmail.com>
2022-08-14 10:05:59 -0400

    feat(destini): initial commit

---
+++ Destini-iOS13/Model/Stories.swift
@@ -0,0 +1,16 @@
+//
+//  Stories.swift
+//  Destini-iOS13
+//
+//  Created by Sidney So on 10/11/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+let stories = [
+    Tale(title:"You see a fork in the road.", choice1:"Take a left.", choice2: "Take a right."),
+    Tale(title:"You see a tiger.", choice1:"Shout for help.", choice2: "Play dead."),
+    Tale(title:"You find a treasure chest.", choice1:"Open it.", choice2: "Check for traps."),
+    
+]
---
+++ Destini-iOS13/Model/Tale.swift
@@ -0,0 +1,15 @@
+//
+//  Tale.swift
+//  Destini-iOS13
+//
+//  Created by Sidney So on 10/11/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Tale {
+    let title: String
+    let choice1: String
+    let choice2: String
+}
